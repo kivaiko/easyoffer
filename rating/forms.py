9ec8230e2cfd00abc4d2af64bffb76650450e403
Alex Kivaiko <alexagree1@gmail.com>
2023-08-14 16:21:46 +0400

    Correct design for forms

--- rating/forms.py
+++ rating/forms.py
@@ -8,18 +8,20 @@ class CommentForm(forms.Form):
 
 
 class AddQuestion(forms.Form):
-    title = forms.CharField(max_length=255, label='Вопрос:')
-    tag = forms.ModelChoiceField(queryset=Tag.objects.all(), empty_label=None, label='Тег:')
+    title = forms.CharField(max_length=255, label='Вопрос:', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    tag = forms.ModelChoiceField(queryset=Tag.objects.all(), empty_label=None, label='Тег:',
+                                 widget=forms.Select(attrs={'class': 'form-select'}))
 
 
 class VideoAnswerForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Название видео: ')
-    url = forms.URLField(label='Ссылка на момент ответа: ')
+    title = forms.CharField(max_length=255, label='Название видео: ',
+                            widget=forms.TextInput(attrs={'class': 'form-control'}))
+    url = forms.URLField(label='Ссылка на момент ответа: ', widget=forms.URLInput(attrs={'class': 'form-control'}))
 
 
 class ExtraContentForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Название: ')
-    url = forms.URLField(label='Ссылка: ')
+    title = forms.CharField(max_length=255, label='Название: ', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    url = forms.URLField(label='Ссылка: ', widget=forms.URLInput(attrs={'class': 'form-control'}))
 
 
 class MockForm(forms.ModelForm):
