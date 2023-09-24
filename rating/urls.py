9358d66e2847cd39c4810daf2ed5ab4fab5ec67d
Alex Kivaiko <alexagree1@gmail.com>
2023-09-24 16:10:03 +0400

    Refactor func rating views to class

--- rating/urls.py
+++ rating/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from rating import views
-from rating.views import IndexView, ThxView, ThxQuizView, ThxAccessSuccessView
+from rating.views import IndexView, ThxView, ThxQuizView, ThxAccessSuccessView, ProfessionView, QuestionView, QuizView,\
+    MockView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('thx_data', ThxView.as_view(), name='thx_data'),
     path('access_success', ThxAccessSuccessView.as_view(), name='access_success'),
-    path('mock', views.mock, name='mock'),
+    path('mock', MockView.as_view(), name='mock'),
     path('access', views.access, name='access'),
-    path('question/<int:question_id>', views.question, name='question'),
+    path('question/<int:question_id>', QuestionView.as_view(), name='question'),
     path('quiz/thx_quiz', ThxQuizView.as_view(), name='thx_quiz'),
-    path('quiz/<slug:prof_slug>', views.quiz, name='quiz'),
-    path('rating/<slug:slug>', views.profession, name='question_rating'),
+    path('quiz/<slug:prof_slug>', QuizView.as_view(), name='quiz'),
+    path('rating/<slug:slug>', ProfessionView.as_view(), name='question_rating'),
 ]
--- rating/views.py
+++ rating/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, redirect
-from datetime import datetime, timedelta
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView
-from django.db.models import F, Value, Q
-from django.core.paginator import Paginator
+from django.views import View
 from .forms import *
 from .models import *
-from .service import get_access_status, get_client_ip, get_prof_data, get_ratings, get_filtered_mocks, get_pagination, \
+from .service import get_access_status, get_prof_data, get_ratings, get_filtered_mocks, get_pagination, \
     get_question_content, create_answer, create_video_link, create_extra_link, get_data_from_content_form, \
     update_rating_from_quiz, create_mock, crete_question, giving_access
 
@@ -18,34 +16,35 @@ class IndexView(ListView):
     queryset = Profession.objects.filter(public_rating=True)
 
 
-def profession(request, slug):
-    if request.method == "POST":
+class ProfessionView(View):
+    def post(self, request, slug):
         form = AddQuestion(request.POST)
         if form.is_valid():
             crete_question(form, slug)
             return redirect('thx_data')
 
-    tag = request.GET.get("tag")
-    search_form, add_question_form = QuestionSearchForm(request.GET), AddQuestion()
-    prof_data, available_tags, questions_amount = get_prof_data(slug)
-    ratings = get_ratings(tag, search_form, prof_data)
-    access_status = get_access_status(request)
-    page_obj = get_pagination(request, ratings)
-    return render(request, 'question_rating.html', {
-        'prof_data': prof_data,
-        'available_tags': available_tags,
-        'ratings': ratings,
-        'questions_amount': questions_amount,
-        'page_obj': page_obj,
-        'form': add_question_form,
-        'search_form': search_form,
-        'access': access_status,
-        'tag': tag,
-    })
+    def get(self, request, slug):
+        tag = request.GET.get("tag")
+        search_form, add_question_form = QuestionSearchForm(request.GET), AddQuestion()
+        prof_data, available_tags, questions_amount = get_prof_data(slug)
+        ratings = get_ratings(tag, search_form, prof_data)
+        access_status = get_access_status(request)
+        page_obj = get_pagination(request, ratings)
+        return render(request, 'question_rating.html', {
+            'prof_data': prof_data,
+            'available_tags': available_tags,
+            'ratings': ratings,
+            'questions_amount': questions_amount,
+            'page_obj': page_obj,
+            'form': add_question_form,
+            'search_form': search_form,
+            'access': access_status,
+            'tag': tag,
+        })
 
 
-def question(request, question_id):
-    if request.method == "POST":
+class QuestionView(View):
+    def post(self, request, question_id):
         comment_form, video_form, extra_content_form = get_data_from_content_form(request)
         if comment_form.is_valid():
             create_answer(question_id, comment_form)
@@ -57,30 +56,33 @@ def question(request, question_id):
             create_extra_link(question_id, extra_content_form)
             return redirect('thx_data')
 
-    comment_form, video_answer_form, extra_content_form = CommentForm(), VideoAnswerForm(prefix='video'),\
-                                                          ExtraContentForm(prefix='content')
-    question_data, answers, video_links, extra_links = get_question_content(question_id)
-    return render(request, 'question.html', {
-        'comment_form': comment_form,
-        'video_answer_form': video_answer_form,
-        'extra_content_form': extra_content_form,
-        'question_data': question_data,
-        'answers': answers,
-        'video_links': video_links,
-        'extra_links': extra_links,
-    })
+    def get(self, request, question_id):
+        comment_form, video_answer_form, extra_content_form = CommentForm(), VideoAnswerForm(prefix='video'),\
+                                                              ExtraContentForm(prefix='content')
+        question_data, answers, video_links, extra_links = get_question_content(question_id)
+        return render(request, 'question.html', {
+            'comment_form': comment_form,
+            'video_answer_form': video_answer_form,
+            'extra_content_form': extra_content_form,
+            'question_data': question_data,
+            'answers': answers,
+            'video_links': video_links,
+            'extra_links': extra_links,
+        })
 
 
-def quiz(request, prof_slug):
-    if request.method == "POST":
+class QuizView(View):
+    def post(self, request, prof_slug):
         update_rating_from_quiz(request, prof_slug)
         return redirect('thx_quiz')
-    prof_data = Profession.objects.get(slug=prof_slug)
-    ratings = Rating.objects.select_related('question').filter(profession=prof_data, public=True).order_by("-rating")
-    return render(request, 'quiz.html', {
-        'prof_data': prof_data,
-        'ratings': ratings,
-    })
+
+    def get(self, request, prof_slug):
+        prof_data = Profession.objects.get(slug=prof_slug)
+        ratings = Rating.objects.select_related('question').filter(profession=prof_data, public=True).order_by("-rating")
+        return render(request, 'quiz.html', {
+            'prof_data': prof_data,
+            'ratings': ratings,
+        })
 
 
 class ThxQuizView(TemplateView):
@@ -98,30 +100,25 @@ class ThxAccessSuccessView(TemplateView):
     template_name = 'access_success.html'
 
 
-class MockView(ListView):
-    """Список мок-интервью"""
-    template_name = 'mock.html'
-    model = MockInterview
-    queryset = MockInterview.objects.filter(public=True)
-
-
-def mock(request):
-    if request.method == "POST":
+class MockView(View):
+    def post(self, request):
         form = AddMockForm(request.POST)
         if form.is_valid():
             create_mock(form)
             return redirect('thx_data')
-    mocks, profession_id, grade = get_filtered_mocks(request)
-    page_obj = get_pagination(request, mocks)
-    form_filter, form = MockFilterForm, AddMockForm
-    return render(request, 'mock.html', {
-        'mocks': mocks,
-        'page_obj': page_obj,
-        'form_filter': form_filter,
-        'form': form,
-        'profession_id': profession_id,
-        'grade': grade,
-    })
+
+    def get(self, request):
+        mocks, profession_id, grade = get_filtered_mocks(request)
+        page_obj = get_pagination(request, mocks)
+        form_filter, form = MockFilterForm, AddMockForm
+        return render(request, 'mock.html', {
+            'mocks': mocks,
+            'page_obj': page_obj,
+            'form_filter': form_filter,
+            'form': form,
+            'profession_id': profession_id,
+            'grade': grade,
+        })
 
 
 def access(request):
