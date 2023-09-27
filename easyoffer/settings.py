aa1316de27bba1c68875a81e40725831ca320e74
Alex Kivaiko <alexagree1@gmail.com>
2023-09-27 15:08:37 +0400

    Change timezone in settings

--- easyoffer/settings.py
+++ easyoffer/settings.py
@@ -68,9 +68,6 @@ INTERNAL_IPS = [
     'localhost',
 ]
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
@@ -105,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+3'
 USE_I18N = True
 USE_TZ = True
 
