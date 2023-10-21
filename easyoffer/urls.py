90752465527b5275c5eba83f5d68805561ee42be
Alex Kivaiko <alexagree1@gmail.com>
2023-10-21 18:10:06 +0400

    Change admin url

--- easyoffer/urls.py
+++ easyoffer/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('my/admin/', admin.site.urls),
     path('', include('analytic.urls')),
     path('', include('access.urls')),
     path('', include('mentor.urls')),
