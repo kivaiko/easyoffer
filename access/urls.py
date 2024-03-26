99f9b63774d7762e01865bc0f3b3bb088de1c09a
Alex Kivaiko <alexagree1@gmail.com>
2024-03-26 07:55:42 +0400

    Change access2

--- access/urls.py
+++ access/urls.py
@@ -3,7 +3,7 @@ from access.views import ThxAccessSuccessView, AccessView, ThxAccessSuccessOldVi
 
 urlpatterns = [
     path('access_success', ThxAccessSuccessView.as_view(), name='access_success'),
-    path('access_old', ThxAccessSuccessOldView.as_view(), name='access_old'),
+    path('access_old', ThxAccessSuccessOldView.as_view(), name='access_success_old'),
     path('NDTOIzOclYmcKzvzWAJVlYmcKzvOclDTO', AccessView.as_view(), name='access'),
-    path('Jljo0U9Erg7QDtkb133G7vzDexHVw6Iz', AccessOldView.as_view(), name='access_success_old'),
+    path('Jljo0U9Erg7QDtkb133G7vzDexHVw6Iz', AccessOldView.as_view(), name='access_old'),
 ]
